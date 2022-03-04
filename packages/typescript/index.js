aa2b775684a406d91a335202a545f71057efad25
protagonisths <13837050616@163.com>
2022-03-04 11:08:06 +0800

    feat(project): init project struct

---
+++ packages/typescript/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'import/named': 'off',
+    '@typescript-eslint/semi': ['error', 'never'],
+    '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
+    '@typescript-eslint/member-delimiter-style': ['error', { multiline: { delimiter: 'none' } }],
+    '@typescript-eslint/type-annotation-spacing': ['error', {}],
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', disallowTypeAnnotations: false }],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/consistent-indexed-object-style': ['error', 'record'],
+    '@typescript-eslint/prefer-ts-expect-error': 'error',
+  },
+}
