d8c12349647af03683524c8a10328ab0458c72fa
Ruster <13837050616@163.com>
2025-04-03 14:30:17 +0800

    feat(shared): add func for type

--- packages/shared/src/type.ts
+++ packages/shared/src/type.ts
@@ -1,28 +1,28 @@
 enum Types {
-  Array = '[object Array]',
-  Object = '[object Object]',
-  String = '[object String]',
-  Number = '[object Number]',
-  Boolean = '[object Boolean]',
-  Function = '[object Function]',
-  Symbol = '[object Symbol]',
-  Date = '[object Date]',
-  RegExp = '[object RegExp]',
-  Error = '[object Error]',
-  Promise = '[object Promise]',
-  Map = '[object Map]',
-  Set = '[object Set]',
-  WeakMap = '[object WeakMap]',
-  WeakSet = '[object WeakSet]',
-  BigInt = '[object BigInt]',
-  Undefined = '[object Undefined]',
-  Null = '[object Null]',
-  BigInt64Array = '[object BigInt64Array]',
-  BigUint64Array = '[object BigUint64Array]',
-  Float32Array = '[object Float32Array]',
-  Float64Array = '[object Float64Array]',
-  Int8Array = '[object Int8Array]',
-  Int16Array = '[object Int16Array]'
+  '[object Array]' = 'Array',
+  '[object Object]' = 'Object', 
+  '[object String]' = 'String',
+  '[object Number]' = 'Number',
+  '[object Boolean]' = 'Boolean',
+  '[object Function]' = 'Function',
+  '[object Symbol]' = 'Symbol',
+  '[object Date]' = 'Date',
+  '[object RegExp]' = 'RegExp',
+  '[object Error]' = 'Error',
+  '[object Promise]' = 'Promise',
+  '[object Map]' = 'Map',
+  '[object Set]' = 'Set',
+  '[object WeakMap]' = 'WeakMap',
+  '[object WeakSet]' = 'WeakSet',
+  '[object BigInt]' = 'BigInt',
+  '[object Undefined]' = 'Undefined',
+  '[object Null]' = 'Null',
+  '[object BigInt64Array]' = 'BigInt64Array',
+  '[object BigUint64Array]' = 'BigUint64Array',
+  '[object Float32Array]' = 'Float32Array',
+  '[object Float64Array]' = 'Float64Array',
+  '[object Int8Array]' = 'Int8Array',
+  '[object Int16Array]' = 'Int16Array'
 }
 
 const isType = (type: unknown) => {
@@ -30,12 +30,126 @@ const isType = (type: unknown) => {
   return Types[typeStr as keyof typeof Types]
 }
 
-isType([])
+const isArray = (arg: unknown): boolean => {
+  return isType(arg) === 'Array'
+}
 
-const isArr = () => {
+const isObject = (arg: unknown): boolean => {
+  return isType(arg) === 'Object'
+}
 
+const isString = (arg: unknown): boolean => {
+  return isType(arg) === 'String'
+}
+
+const isNumber = (arg: unknown): boolean => {
+  return isType(arg) === 'Number'
+}
+
+const isBoolean = (arg: unknown): boolean => {
+  return isType(arg) === 'Boolean'
+}
+
+const isFunction = (arg: unknown): boolean => {
+  return isType(arg) === 'Function'
+}
+
+const isSymbol = (arg: unknown): boolean => {
+  return isType(arg) === 'Symbol'
+}
+
+const isDate = (arg: unknown): boolean => {
+  return isType(arg) === 'Date'
+}
+
+const isRegExp = (arg: unknown): boolean => {
+  return isType(arg) === 'RegExp'
+}
+
+const isError = (arg: unknown): boolean => {
+  return isType(arg) === 'Error'
+}
+
+const isPromise = (arg: unknown): boolean => {
+  return isType(arg) === 'Promise'
+}
+
+const isMap = (arg: unknown): boolean => {
+  return isType(arg) === 'Map'
+}
+
+const isSet = (arg: unknown): boolean => {
+  return isType(arg) === 'Set'
+}
+
+const isWeakMap = (arg: unknown): boolean => {
+  return isType(arg) === 'WeakMap'
+}
+
+const isWeakSet = (arg: unknown): boolean => {
+  return isType(arg) === 'WeakSet'
+}
+
+const isBigInt = (arg: unknown): boolean => {
+  return isType(arg) === 'BigInt'
+}
+
+const isUndefined = (arg: unknown): boolean => {
+  return isType(arg) === 'Undefined'
+}
+
+const isNull = (arg: unknown): boolean => {
+  return isType(arg) === 'Null'
+}
+
+const isBigInt64Array = (arg: unknown): boolean => {
+  return isType(arg) === 'BigInt64Array'
+}
+
+const isBigUint64Array = (arg: unknown): boolean => {
+  return isType(arg) === 'BigUint64Array'
+}
+
+const isFloat32Array = (arg: unknown): boolean => {
+  return isType(arg) === 'Float32Array'
+}
+
+const isFloat64Array = (arg: unknown): boolean => {
+  return isType(arg) === 'Float64Array'
+}
+
+const isInt8Array = (arg: unknown): boolean => {
+  return isType(arg) === 'Int8Array'
+}
+
+const isInt16Array = (arg: unknown): boolean => {
+  return isType(arg) === 'Int16Array'
 }
 
 export {
-  isArr
+  isArray,
+  isObject,
+  isString,
+  isNumber,
+  isBoolean,
+  isFunction,
+  isSymbol,
+  isDate,
+  isRegExp,
+  isError,
+  isPromise,
+  isMap,
+  isSet,
+  isWeakMap,
+  isWeakSet,
+  isBigInt,
+  isUndefined,
+  isNull,
+  isBigInt64Array,
+  isBigUint64Array,
+  isFloat32Array,
+  isFloat64Array,
+  isInt8Array,
+  isInt16Array,
+  isType
 }
